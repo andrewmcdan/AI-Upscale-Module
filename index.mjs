be71dd166d86c83b6248f8236edfdd1589950696
Andrew <andrewmcdan@gmail.com>
2023-10-13 17:15:37 -0400

    working on downlaoder

--- index.mjs
+++ index.mjs
@@ -3,9 +3,17 @@ import AdmZip from 'adm-zip';
 import fetch from 'node-fetch';
 import { exec, execFile, execSync } from 'child_process';
 import { stdout } from 'process';
+import WebFileDownloader from 'web-file-downloader';
+
+const downloaderOpts = {
+    maxDownloadConnect: 5,
+    fileChunkSize: 5 * 1024 * 1024
+};
+
+const downloader = new WebFileDownloader(downloaderOpts);
 
 // TODO: rewrite this into a class
-class Upscaler{
+class Upscaler {
     constructor(options) {
         this.options = options;
     }
@@ -37,12 +45,12 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
         fs.mkdirSync(outputPath);
     }
 
-    if(format !== "jpg" && format !== "png") {
+    if (format !== "jpg" && format !== "png") {
         console.error('Format is not supported');
         return false;
     }
 
-    if(scale !== 2 && scale !== 3 && scale !== 4) {
+    if (scale !== 2 && scale !== 3 && scale !== 4) {
         console.error('Scale is not supported');
         return false;
     }
@@ -52,30 +60,30 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
     let upscalerPath = "";
 
     // find upscaler
-    if(fs.existsSync('./unzipped')) {
+    if (fs.existsSync('./unzipped')) {
         let unzippedFolder = fs.readdirSync('./unzipped');
         let latestVersion = {};
         latestVersion.versionNo = 0;
         latestVersion.folderName = "";
-        if(unzippedFolder.length !== 0) {
-            unzippedFolder.forEach((file,i) => {
-                let versionNo = file.substring(file.indexOf('kan-v') + 5,file.lastIndexOf("-"));
+        if (unzippedFolder.length !== 0) {
+            unzippedFolder.forEach((file, i) => {
+                let versionNo = file.substring(file.indexOf('kan-v') + 5, file.lastIndexOf("-"));
                 let versionNoArray = versionNo.split('.');
                 let versionNoInt = 0;
-                versionNoArray.forEach((version,index) => {
-                    versionNoInt += parseInt(version) * Math.pow(10 , index * 3);
+                versionNoArray.forEach((version, index) => {
+                    versionNoInt += parseInt(version) * Math.pow(10, index * 3);
                 });
-                if(versionNoInt > latestVersion.versionNo) {
+                if (versionNoInt > latestVersion.versionNo) {
                     latestVersion.versionNo = versionNoInt;
                     latestVersion.folderName = file;
                 }
             });
         }
-        if(latestVersion.folderName !== "") {
+        if (latestVersion.folderName !== "") {
             let dir = `./unzipped/${latestVersion.folderName}/`;
             let files = fs.readdirSync(dir);
             let exeFile = files.find(file => file.endsWith('.exe'));
-            if(exeFile !== undefined) {
+            if (exeFile !== undefined) {
                 upscalerFound = true;
                 upscalerPath = dir + exeFile;
             }
@@ -84,39 +92,39 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
 
     // find upscale model
     let modelsFound = false;
-    let modelsFolder= "";
-    if(upscalerFound) {
+    let modelsFolder = "";
+    if (upscalerFound) {
         // chekc to make sure model folder exists
-        if(fs.existsSync('./models')) {
+        if (fs.existsSync('./models')) {
             let modelFolder = fs.readdirSync('./models');
-            if(modelFolder.length !== 0) {
-                modelFolder.forEach((file,i) => {
+            if (modelFolder.length !== 0) {
+                modelFolder.forEach((file, i) => {
                     let folder1 = null;
-                    try{
+                    try {
                         folder1 = fs.readdirSync('./models/' + file);
-                    }catch(e) {
+                    } catch (e) {
                         // do nothing
                     }
                     // check to see if "file" is a .param or .bin file
-                    if(file.endsWith('.param') || file.endsWith('.bin')) {
+                    if (file.endsWith('.param') || file.endsWith('.bin')) {
                         modelsFound = true;
                         modelsFolder = './models/';
-                    }else if (folder1 !== undefined) {
-                        folder1.forEach((file2,i) => {
+                    } else if (folder1 !== undefined) {
+                        folder1.forEach((file2, i) => {
                             let folder2 = null;
-                            try{
+                            try {
                                 folder2 = fs.readdirSync('./models/' + file + '/' + file2);
-                            }catch(e) {
+                            } catch (e) {
                                 // do nothing
                             }
                             // check to see if "file" is a .param or .bin file
-                            if(file2.endsWith('.param') || file2.endsWith('.bin')) {
+                            if (file2.endsWith('.param') || file2.endsWith('.bin')) {
                                 modelsFound = true;
                                 modelsFolder = './models/' + file + '/';
-                            }else if (folder2 !== undefined) {
-                                folder2.forEach((file3,i) => {
+                            } else if (folder2 !== undefined) {
+                                folder2.forEach((file3, i) => {
                                     // check to see if "file" is a .param or .bin file
-                                    if(file3.endsWith('.param') || file3.endsWith('.bin')) {
+                                    if (file3.endsWith('.param') || file3.endsWith('.bin')) {
                                         modelsFound = true;
                                         modelsFolder = './models/' + file + '/' + file2 + '/';
                                     }
@@ -140,17 +148,17 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
         }
         console.error('Upscaler is not installed. Attempting to aquire.');
         let platform = process.platform;
-        if(platform === 'win32') {
+        if (platform === 'win32') {
             // download windows upscaler
             //let upscaler = await fetch('https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-windows.zip');
             //unzip upscaler
             // log step
             console.log('Downloading and unzipping upscaler');
             let success = await downloadAndUnzip('https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-windows.zip',
-            'zipped/realesrgan-ncnn-vulkan-v0.2.0-windows.zip','unzipped/');
-            if(success) {
+                'zipped/realesrgan-ncnn-vulkan-v0.2.0-windows.zip', 'unzipped/');
+            if (success) {
                 console.log('Successfully downloaded and unzipped upscaler');
-            }else{
+            } else {
                 return false;
             }
         } else if (platform === 'darwin') {
@@ -165,14 +173,14 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
         }
     }
 
-    if(!modelsFound) {
+    if (!modelsFound) {
         // download models
         // log step
         console.log('Downloading and unzipping models');
-        let success = await downloadAndUnzip('https://github.com/upscayl/custom-models/archive/refs/heads/main.zip','zipped/main.zip','unzipped/');
-        if(!success) {
+        let success = await downloadAndUnzip('https://github.com/upscayl/custom-models/archive/refs/heads/main.zip', 'zipped/main.zip', 'unzipped/');
+        if (!success) {
             //move unzipped folder to models folder
-            fs.renameSync('./unzipped/custom-models-main','./models/custom-models-main');
+            fs.renameSync('./unzipped/custom-models-main', './models/custom-models-main');
             return false;
         }
     }
@@ -183,7 +191,7 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
     }
 
     // run upscaler
-    
+
     // resolve absolute paths
     upscalerPath = fs.realpathSync(upscalerPath);
     inputFile = fs.realpathSync(inputFile);
@@ -198,7 +206,7 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
     execString += " -n ultrasharp-2.0.1 ";
     let scaling = exec(execString, (err, stdout, stderr) => {
         if (err) {
-           // console.error(err);
+            // console.error(err);
             return false;
         }
         //console.log(stdout);
@@ -209,14 +217,14 @@ const upscale = async (inputFile, outputPath = null, format = "jpg", scale = 4) 
         //console.error(`stderr: ${data}`);
         // TODO: call progress callback
     });
-    while(scaling.exitCode === null) {
+    while (scaling.exitCode === null) {
         await waitSeconds(5);
     }
     // log
     //console.log('Successfully upscaled image');
-    if(scaling.exitCode == 0) {
+    if (scaling.exitCode == 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -234,26 +242,26 @@ async function waitSeconds(count) {
 async function downloadAndUnzip(url, zipPath, extractPath) {
     try {
         // Fetching the zip file
-        const response = await fetch(url);
-        // const totalSize = response.headers.get('content-length');
-        // if(isNaN(totalSize)) {
-        //     console.error('Failed to download and unzip: ' + error);
-        //     return false;
-        // }
-
-        // const fileStream = fs.createWriteStream(zipPath);
-
+        // const response = await fetch(url);
 
         // Getting the buffer of the downloaded file
-        console.log('Downloading zip file1');
-        const arrayBuffer = await response.arrayBuffer();
-        console.log('Downloaded zip file2');
-        const buffer = Buffer.from(arrayBuffer);
+        // console.log('Downloading zip file1');
+        // const arrayBuffer = await response.arrayBuffer();
+        // console.log('Downloaded zip file2');
+        // const buffer = Buffer.from(arrayBuffer);
         // Saving the zip file to the disk
         fs.writeFileSync(zipPath, buffer);
+
+        const fileInfo = downloader.download(url, zipPath);
+
+        downloader.on('progress', (data) => {
+            console.log('Download progress: ', data);
+        });
+
         // Unzipping the downloaded file
-        const zip = new AdmZip(zipPath);
-        zip.extractAllTo(extractPath, true);
+        // const zip = new AdmZip(zipPath);
+        // zip.extractAllTo(extractPath, true);
+        await waitSeconds(60);
         return true;
     } catch (error) {
         // log step
