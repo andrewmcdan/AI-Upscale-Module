d5ff94d2b26cea5af9eb6c40e24d116aa8888fc0
Andrew <andrewmcdan@gmail.com>
2023-12-13 23:12:50 -0500

    df

--- index.js
+++ index.js
@@ -564,6 +564,7 @@ class Upscaler {
                         if ((inFileName.includes(inputFileTemp) && outFileName.includes(outputFileTemp)) || (inputFileTemp.includes(inFileName) && outputFileTemp.includes(outFileName))) {
                             this.scalingsInprogress--;
                             resolve(true);
+                            await waitSeconds(5);
                             console.log("Upscaler finished");
                             if (this.nextToProcess.length > 0 && this.scalingsInprogress == 0 && this.scalerExec !== null) {
                                 // do next jobs
